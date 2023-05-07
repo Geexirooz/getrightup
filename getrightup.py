cff44417c9b701a360921f4b8547b76a95fb6951
Geexirooz <roozbehjamali1995@gmail.com>
2023-05-07 14:49:29 +0100

    standardise the code and add discord option

--- getrightup.py
+++ getrightup.py
@@ -2,39 +2,105 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from time import sleep  # this should go at the top of the file
 from bs4 import BeautifulSoup
+import json
+import requests
+import sys
+import argparse
 
-# Options set
-options = Options()
-options.add_argument("--headless")
-
-# Get the page
-browser = webdriver.Firefox(options=options)
-browser.get("https://pentester.land/writeups/")
-
-# wait until it's processed
-sleep(5)
-
-# get the rendered source code
-src = browser.execute_script(
-    "return document.getElementsByTagName('html')[0].innerHTML"
+parser = argparse.ArgumentParser(
+    "bountydog.py",
+    formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=40),
 )
 
+parser.add_argument(
+    "-d",
+    "--discord",
+    help="Discord's channel Webhook",
+    dest="webhook",
+    default=sys.maxsize,
+    required=True,
+    type=str,
+)
 
-soup = BeautifulSoup(src, "html.parser")
-odd_trs = soup.find_all("tr", {"class": "odd"})
-even_trs = soup.find_all("tr", {"class": "even"})
-lst_of_writeups = []
-for odd_tr in odd_trs:
-    # The following gives a list
-    writeup = odd_tr.find("a").contents
-    # let's add the link to the list
-    writeup.append(odd_tr.find("a")["href"])
-    lst_of_writeups.append(writeup)
+args = parser.parse_args()
 
 
-for even_tr in even_trs:
-    writeup = even_tr.find("a").contents
-    writeup.append(even_tr.find("a")["href"])
-    lst_of_writeups.append(writeup)
+def get_pentesterland_rendered_source() -> str:
+    # set headless browser option
+    options = Options()
+    options.add_argument("--headless")
+    # Get the page
+    browser = webdriver.Firefox(options=options)
+    browser.get("https://pentester.land/writeups/")
+    # wait until it's fully rendered
+    sleep(5)
+    # get the rendered source code via executing a javascript code
+    src = browser.execute_script(
+        "return document.getElementsByTagName('html')[0].innerHTML"
+    )
+    browser.close
 
-print(len(lst_of_writeups))
+    return src
+
+
+def get_trs(src) -> list:
+    trs = src.find("tbody")
+    lst_os_writeups = []
+    for tr in trs.children:
+        writeup = tr.find("a").contents
+        writeup.append(tr.find("a")["href"])
+        lst_os_writeups.append(writeup)
+
+    return lst_os_writeups
+
+
+def discordit(msg: str, webhook: str) -> None:
+    # Discord does not allow sending a message containing more than 2000 chars
+    if len(msg) > 2000:
+        num_of_chunks = len(msg) // 1900 + 1
+        lst = msg.split("\n")
+        sized_msg = ""
+        for i in range(num_of_chunks):
+            while len(sized_msg) <= 1900 and len(lst) > 0:
+                sized_msg = sized_msg + lst.pop(0) + "\n"
+            if len(lst) != 0:
+                sized_msg = (
+                    sized_msg
+                    + "\n#########################\nTo Be Continued .......\n#########################\n"
+                )
+            data = {"content": sized_msg}
+            requests.post(webhook, json=data)
+    else:
+        data = {"content": msg}
+        requests.post(webhook, json=data)
+
+    return
+
+
+def getrightup():
+    src = BeautifulSoup(get_pentesterland_rendered_source(), "html.parser")
+    new_writeups = get_trs(src)
+
+    with open("writeups.lst") as f:
+        old_writeups = json.load(f)
+
+    newly_released_writeups = [i for i in new_writeups if i not in old_writeups]
+
+    # print(newly_released_writeups)
+    # print(len(newly_released_writeups))
+    # for i in newly_released_writeups:
+    #    print(i)
+    #    print(type(i))
+
+    if newly_released_writeups:
+        for writeup in newly_released_writeups:
+            msg = "{:s}:\n{:s}\n".format(writeup[0], writeup[1])
+            discordit(msg, args.webhook)
+
+        with open("writeups.lst", "w") as f:
+            json.dump(new_writeups, f)
+
+    return
+
+
+getrightup()
